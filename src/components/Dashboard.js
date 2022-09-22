196466eabefc0381e31044dee88671e91cea44e7
Mary Rossi <maryrossi106@gmail.com>
2022-09-22 15:09:58 -0500

    added website visitors

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AverageRating from './AverageRating';
 import Review from './Review';
 import SentimentAnalysis from './SentimentAnalysis';
+import WebsiteVisitors from './WebsiteVisitors';
 
 function Dashboard() {
     return(
@@ -9,6 +10,7 @@ function Dashboard() {
             <AverageRating rating='4.6' />
             <Review reviews='1,281' />
             <SentimentAnalysis sentimentAnalysisOne="960" sentimentAnalysisTwo="122" sentimentAnalysisThree="321" />
+            <WebsiteVisitors websiteVisitors="821" />
         </div>
     );
 };
---
+++ src/components/WebsiteVisitors.js
@@ -0,0 +1,10 @@
+const WebsiteVisitors = (props) => {
+    return(
+        <div className='websiteVisitors'>
+            <h2>Website Visitors</h2>
+            <h3>{props.websiteVisitors}</h3>
+        </div>
+    );
+};
+
+export default WebsiteVisitors;
